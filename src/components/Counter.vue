3b3e850d2c8d64a149a2b7a3eeb75d746870e86e
Mub <mubpersonal5@gmail.com>
2024-07-30 23:21:12 -0500

    New counter component

---
+++ src/components/Counter.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue";
+const counter = ref(0);
+const sum = () => {
+  counter.value++;
+}
+const subtract = () => {
+  counter.value--;
+}
+</script>
+
+<template>
+  <h1>Hola</h1>
+  {{ counter }}
+  <br>
+  <button @click="sum">Sumar</button>
+  <button @click="subtract">Restar</button>
+</template>
+
+<style scoped></style>
