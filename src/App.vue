e6b837dcf2a29af71f25bfb0c38cb93aa1113dd5
Mub <mubpersonal5@gmail.com>
2024-07-30 23:41:53 -0500

    New component for adding a task to the task list

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Counter from './components/Counter.vue';
+import TaskList from './components/TaskList.vue';
 </script>
 
 <template>
   <Counter></Counter>
+  <TaskList></TaskList>
 </template>
 
 <style scoped></style>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+const tasks = ref([]);
+const newTask = ref('');
+const addTask = () => {
+    tasks.value.push(newTask.value);
+    newTask.value = '';
+}
+</script>
+
+<template>
+    <div>
+        <h2>List tasks</h2>
+        <input type="text" v-model="newTask" @keyup.enter="addTask">
+        <ul>
+            <li v-for="(task, index) in tasks" :key="index">{{ task }}</li>
+        </ul>
+    </div>
+</template>
+
+<style scoped></style>
