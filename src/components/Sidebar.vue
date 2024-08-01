1e961d34af60d56402cb3bdc498e59d04d75bce1
Mub <mubpersonal5@gmail.com>
2024-07-31 22:01:22 -0500

    Create store with pinia for the register view

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,9 +1,22 @@
-<!-- eslint-disable vue/multi-word-component-names -->
-<script setup></script>
+<script setup>
+import { useRegisterStore } from '@/modules/register/stores/registerStore';
+import { ref, watch } from 'vue';
+const registerStore = useRegisterStore();
+const name = ref(registerStore.name.value);
+const email = ref(registerStore.email.value);
+watch(() => registerStore.name, (newValue) => {
+    name.value = newValue;
+});
+watch(() => registerStore.email, (newValue) => {
+    email.value = newValue;
+});
+</script>
 
 <template>
     <div class="sidebar">
-        <h2>Vue - Test</h2>
+        <h2>Bienvenido</h2>
+        <h3>Nombre: {{ name }}</h3>
+        <h3>Email: {{ email }}</h3>
         <ul>
             <li><router-link to="/Counter">Counter</router-link></li>
             <li><router-link to="/taskList">Task List</router-link></li>
--- src/modules/register/views/registerView.vue
+++ src/modules/register/views/registerView.vue
@@ -1,25 +1,30 @@
 <script setup>
 import { Form, Field, ErrorMessage } from 'vee-validate';
 import schema from '../schemas/validationSchema';
+import { useRegisterStore } from '../stores/registerStore';
+import { ref } from 'vue';
+const registerStore = useRegisterStore();
+const name = ref('');
+const email = ref('');
 
 const onSubmit = () => {
+    registerStore.saveRegister(name.value, email.value);
     console.log('success');
 }
-
 </script>
 
 <template>
     <div>
         <h2>Formulario de registro</h2>
-        <Form :validation-schema="schema" @submit="onSubmit">
+        <Form :validation-schema="schema" @submit="onSubmit()">
             <div class="form-group">
                 <label for="name">Nombre</label>
-                <Field type="text" placeholder="Nombre..." name="name" />
+                <Field type="text" placeholder="Nombre..." name="name" v-model="name" />
                 <ErrorMessage name="name"></ErrorMessage>
             </div>
             <div class="form-group">
                 <label for="email">Email</label>
-                <Field type="email" placeholder="Email..." name="email" />
+                <Field type="email" placeholder="Email..." name="email" v-model="email" />
                 <ErrorMessage name="email"></ErrorMessage>
             </div>
             <button type="submit">Register</button>
